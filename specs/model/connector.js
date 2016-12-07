0a67bf95a5d70fc5dfffafbc12618e8c8113a947
vladimir-trifonov <trifonov.dev@gmail.com>
2016-12-07 15:14:16 +0200

    [ARRCONN-227] findOneAPI/findByIDAPI unit tests added

--- specs/model/connector.js
+++ specs/model/connector.js
@@ -20,17 +20,59 @@ module.exports = function () {
 				},
 				connector: Connector
 			});
-			var instance = User.instance({name: "jeff"});
+			var instance = User.instance({ name: "jeff" });
 			var collection = new orm.Collection(User, [instance]);
 			should(collection).be.an.object;
 			should(collection.length).be.equal(1);
-			collection.add(User.instance({name: "nolan"}));
+			collection.add(User.instance({ name: "nolan" }));
 			should(collection.length).be.equal(2);
 			collection.add([
-				User.instance({name: "rick"}),
-				User.instance({name: "tony"})
+				User.instance({ name: "rick" }),
+				User.instance({ name: "tony" })
 			]);
 			should(collection.length).be.equal(4);
+		});
+
+		it('should create findOneAPI with older connector interface', function () {
+			function MockConnector(config) {
+				this.name = 'FindOneDummy';
+				orm.Connector.apply(this, arguments);
+			}
+			util.inherits(MockConnector, orm.Connector);
+			MockConnector.prototype.findOne = function () { };
+			var connector = new MockConnector();
+
+			var User = orm.Model.define('user', {
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: connector
+			});
+
+			connector.emit('init-model', User);
+
+			should.exist(User.findOneAPI);
+		});
+
+		it('should has findByIDAPI with newer connector interface', function () {
+			var connector = new orm.MemoryConnector();
+
+			var User = orm.Model.define('user', {
+				fields: {
+					name: {
+						type: String,
+						required: false
+					}
+				},
+				connector: connector
+			});
+
+			connector.emit('init-model', User);
+
+			should.exist(User.findByIDAPI);
 		});
 
 	});
